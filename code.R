749fced184eff53a2e80d067ef8df4e4320a2fb4
aXaile <aaloisi@etud.insa-toulouse.fr>
2018-12-13 20:02:15 +0100

    recuperation data une seule participation compte par JO

--- code.R
+++ code.R
@@ -1,8 +1,6 @@
-#récupération d'un data frame avec une seule fois chaque participant
-test=cbind(athlete_events$ID,athlete_events$Year,athlete_events$Sex)
-dataUnique=test[!duplicated(test[,1]),]
+#récupération d'un data frame avec une seule fois chaque participant par jeux
 
-#
+dataUnique=unique.data.frame(cbind.data.frame(athlete_events$ID,athlete_events$Year, athlete_events$Sex))
 
 
 
