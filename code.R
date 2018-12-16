9d6e70c2a6c7cd9a6aa3cd2fa2ff0b7ca1b6794f
Laurent Chasserat <laurent.chasserat@gmail.com>
2018-12-16 01:24:11 +0100

    Question 2

--- code.R
+++ code.R
@@ -1,11 +1,32 @@
-#r?cup?ration d'un data frame avec une seule fois chaque participant par jeux
+library("ggplot2", lib.loc="~/R/x86_64-pc-linux-gnu-library/3.4")
+library("plyr", lib.loc="~/R/x86_64-pc-linux-gnu-library/3.4")
+
+### 1 - Nb d'hommes et de femmes dans les JO par an
+
 athlete_events = read.csv(file="./athlete_events.csv", header=TRUE, sep=",")
 
-dataUnique=unique.data.frame(cbind.data.frame(ID=athlete_events$ID,Year=athlete_events$Year, Sex=athlete_events$Sex))
+# Compter les athletes seulement une fois
+year_sex_unique = ddply(athlete_events, .(Sex, Year), summarize, ID=unique(ID))
 
-#dataUniqueOrdonne=dataUnique[order(dataUnique$Year),]
-
-plot1 = ddply(dataUnique, .(Sex, Year), summarize, Nb=sum(Sex==Sex))
+# Prepare la donnee a afficher
+plot1 = ddply(year_sex_unique, .(Sex, Year), summarize, Nb=sum(Sex==Sex))
 
 ggplot(data=plot1, aes(x=Year, y=Nb, fill=Sex)) +
   geom_bar(stat="identity", position=position_dodge())
+
+### Medailles feminines par habitant en 2008, comparees avec l'IDSH des pays 
+
+idsh = read.csv(file="./idsh.csv", header=TRUE, sep=",")
+
+# Medailles feminines avec pays en 2008
+medals_per_year_f = ddply(athlete_events, .(NOC, Year), summarize, Medals=sum(!is.na(Medal) & (Sex=="F")))
+medals_per_year_f_2006_2008 = medals_per_year_f[medals_per_year_f$Year==2006 | medals_per_year_f$Year==2008,]
+
+temp = merge(idsh, medals_per_year_f_2006_2008, by.x="NOC", by.y="NOC")
+plot2 = ddply(temp, .(NOC, idsh2006, Year), summarize, ratio_medals_per_habitants=(Medals/habitants2006))
+plot2$Year = factor(plot2$Year)
+
+ggplot(plot2, aes(x=idsh2006, y=ratio_medals_per_habitants, color=Year)) +
+  geom_point() +
+  scale_color_manual(values=c("#0000ff", "#ff0000"))
+       
